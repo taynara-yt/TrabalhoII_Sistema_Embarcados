aa73f53cabf4434cee8adb15e9aeea45682afeb5
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-01 11:06:02 -0400

    Update exem9.rs

--- exem9.rs
+++ exem9.rs
@@ -6,7 +6,7 @@ fn main() {
      println!("Representação em bits é: {:#018b}", a);
      println!("Representação em bits é: {:#018b}", b);
      
-     println!("A&B é: {:#018b}", (a ^ b));
+     println!("A^B é: {:#018b}", (a ^ b));
      println!("Que corresponde a: {:x}", (a ^ b));
  }
  
