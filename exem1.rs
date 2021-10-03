a06220fef4a79851a501bcb9088121fbc801c2df
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-03 13:08:55 -0400

    finalização

--- exem1.rs
+++ exem1.rs
@@ -2,7 +2,8 @@ fn main(){
     let valor = 0x7ffu16;
     let com = !valor;
     
-    println!("Representação em bits é: {:#018b}", valor);
+    println!("Exemplo(considerando palavra de 16 bits): ");
+    println!("Representação em bits é: {:#018b}", valor);    
     println!("Complemento é: {:b}", com);
     println!("Que corresponde a: {:#x}", com);
 }
