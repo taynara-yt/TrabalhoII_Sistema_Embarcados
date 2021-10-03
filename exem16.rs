a30444eb24174aae12cbf4d79eb58b535c633bc8
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-03 14:37:01 -0400

    final

--- exem16.rs
+++ exem16.rs
@@ -6,7 +6,7 @@ fn main() {
      
      println!("Máscara:");
      println!("Se quisermos inverter os 8 bits mais à esquerda de a e manter os 8 bits mais à direita originais:");
-     println!("Representação de A: {:#x}  = {:#018b}", a, a);
-     println!("Máscara: {:#x} = {:#018b}", masc, masc);
-     println!("Representação de B: {:#x} = {:#018b}", b, b);
+     println!("Representação de A: {:#x}  = {:016b}", a, a);
+     println!("Máscara: {:#x} = {:016b}", masc, masc);
+     println!("Representação de B: {:#x} = {:016b}", b, b);
  }
