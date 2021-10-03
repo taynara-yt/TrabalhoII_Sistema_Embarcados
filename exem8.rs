b59cc9927855eec491578f40775d2ef9d7d34a13
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-03 13:12:18 -0400

    finalização

--- exem8.rs
+++ exem8.rs
@@ -3,10 +3,11 @@ fn main() {
     let a = 0x6DB7u16;
     let b = 0xA726u16;
      
+     println!("Operadores Bitwise Lógicos:");
+     println!("Qual o valor de A & B ?");
      println!("Representação em bits é: {:#018b}", a);
      println!("Representação em bits é: {:#018b}", b);
-     
      println!("A&B é: {:#018b}", (a & b));
-     println!("Que corresponde a: {:x}", (a & b));
+     println!("Que corresponde à: {:x}", (a & b));
  }
  
