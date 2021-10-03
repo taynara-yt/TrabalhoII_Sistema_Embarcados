232b716aa708471c14148d212a8a5c9327845c2a
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-03 13:11:51 -0400

    finalização

--- exem7.rs
+++ exem7.rs
@@ -2,11 +2,11 @@ fn main() {
 
     let b = 0xA726u16;
     
-     
+     println!("Operadores Bitwise Lógicos:");
+     println!("Qual o valor de ~b ?");
      println!("Representação em bits é: {:#018b}", b);
-     println!("a~: {:b}", !b);
-     println!("Que corresponde a: {:x}", !b);
+     println!("b~: {:b}", !b);
+     println!("Que corresponde à: {:x}", !b);
      
  }
- 
  
