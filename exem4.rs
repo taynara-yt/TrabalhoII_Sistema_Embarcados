302e1ef1abfd2ce179d28a2737fd2b17ae7a677b
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-03 13:10:33 -0400

    finalização

--- exem4.rs
+++ exem4.rs
@@ -3,6 +3,7 @@ fn main() {
     let valor = 0xffffu32;
     let com = !valor;
      
+     println!("Exemplo(considerando palavra de 32 bits): ");
      println!("Representação em bits é: {:#032b}", valor);
      println!("Complemento é: {:b}", com);
      println!("Que corresponde a: {:#x}", com);
