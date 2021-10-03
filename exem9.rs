1f3b8f52a5ce2634e9789164393b52da56bf35de
Taynara Silva <66039016+taynara-yt@users.noreply.github.com>
2021-10-03 14:28:10 -0400

    final

--- exem9.rs
+++ exem9.rs
@@ -5,9 +5,8 @@ fn main() {
      
      println!("Operadores Bitwise Lógicos:");
      println!("Qual o valor de A ^ B ?");
-     println!("Representação em bits é: {:#018b}", a);
-     println!("Representação em bits é: {:#018b}", b);
-     println!("A^B é: {:#018b}", (a ^ b));
-     println!("Que corresponde à: {:x}", (a ^ b));
+     println!("Representação em bits é: {:016b}", a);
+     println!("Representação em bits é: {:016b}", b);
+     println!("A^B é: {:016b}", (a ^ b));
+     println!("Que corresponde à: {:#x}", (a ^ b));
  }
- 
